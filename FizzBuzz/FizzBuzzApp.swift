000e387765e6bfb95a5819107fa21335aa231df2
Julian Festing <julfesting@gmail.com>
2023-09-28 14:52:52 +0100

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by julian Festing on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
